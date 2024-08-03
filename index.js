d5f7ef320f1fabc06a401e3afce923c1421796fe
Sajjad Siddiqui <f230620@cfd.nu.edu.pk>
2024-08-03 07:51:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,10 +20,17 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const language = req.headers['accept-language'].split(',')[0];
+  const software = req.headers['user-agent'];
+  
+  res.json({
+      ipaddress: ip,
+      language: language,
+      software: software
+  });
 });
-
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
